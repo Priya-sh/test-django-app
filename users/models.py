054777177cc7c4ebde6579e0458aa173bd730414
Priya Sharma <nicky23397@gmail.com>
2026-03-29 22:32:46 +0530

    first commit

---
+++ users/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=200, blank=True, null=True)
+    code = models.CharField(max_length=6, blank=True, null=True)
+    address = models.CharField(max_length=100, blank=True, null=True)
