f47d045f8f4d05cc6be477886bb18ca8e8eca475
Priya Sharma <nicky23397@gmail.com>
2026-03-29 22:37:11 +0530

    added department model

--- users/models.py
+++ users/models.py
@@ -5,3 +5,7 @@ class Employee(models.Model):
     name = models.CharField(max_length=200, blank=True, null=True)
     code = models.CharField(max_length=6, blank=True, null=True)
     address = models.CharField(max_length=100, blank=True, null=True)
+
+class Department(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    code = models.CharField(max_length=6, blank=True, null=True)
