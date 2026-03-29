3af8795e39191958ae5661c6756789aa0628cfb3
Priya Sharma <nicky23397@gmail.com>
2026-03-29 23:14:34 +0530

    updated employee model

--- users/models.py
+++ users/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class Employee(models.Model):
     name = models.CharField(max_length=200, blank=True, null=True)
-    code = models.CharField(max_length=6, blank=True, null=True)
+    ecode = models.CharField(max_length=6, blank=True, null=True)
     address = models.CharField(max_length=100, blank=True, null=True)
 
 class Department(models.Model):
     name = models.CharField(max_length=100, blank=True, null=True)
-    code = models.CharField(max_length=6, blank=True, null=True)
+    dcode = models.CharField(max_length=6, blank=True, null=True)
